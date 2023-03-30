ff46aab636819289b655a7700160e2ff0f851335
RutujaWaded <129392093+RutujaWaded@users.noreply.github.com>
2023-03-30 18:43:23 +0530

    Update reversepattern.java

--- Rutuja/reversepattern.java
+++ Rutuja/reversepattern.java
@@ -9,9 +9,9 @@ public class reversepattern {
     System.out.print("Enter the no of rows:");
     rows=sc.nextInt();
 
-    for ( int i = rows; i >= 1; --i)
+    for ( int i = rows; i >= 1; i++)
     {
-      for (int j = 1; j <= i; ++j) {
+      for (int j = 1; j <= i; j++) {
         System.out.print("* ");
       }
       System.out.println();
